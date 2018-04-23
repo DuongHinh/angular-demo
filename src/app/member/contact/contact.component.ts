386225fcc14d3bb6eb5bbf68569223ae347d7f31
DuongHinh <duonghinh0402@gmail.com>
2018-04-23 07:38:32 +0700

    using directive and pipe

--- src/app/member/contact/contact.component.ts
+++ src/app/member/contact/contact.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../../../models/contact';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
+
 export class ContactComponent implements OnInit {
+  contact: Contact = {
+    id: 1,
+    firstName: 'Hinh',
+    lastName: 'Duong',
+    phone: '1234567890',
+    email: 'hinhdx@gemvietnam.com'
+  }
+
+  contacts: Contact[] = [
+    {
+      id: 1,
+      firstName: 'Hinh',
+      lastName: 'Duong Xuan',
+      phone: '1234567890',
+      email: 'hinhdx@gemvietnam.com',
+    },
+    {
+      id: 1,
+      firstName: 'Sy',
+      lastName: 'Nguyen Van',
+      phone: '0934908042',
+      email: 'synv@gemvietnam.com'
+    },
+    {
+      id: 1,
+      firstName: 'Binh',
+      lastName: 'Nguyen Thanh',
+      phone: '0914980141',
+      email: 'binhnt@gemvietnam.com'
+    },
+    {
+      id: 1,
+      firstName: 'Dung',
+      lastName: 'Nguyen Ba',
+      phone: '0964970140',
+      email: 'dungnb@gemvietnam.com'
+    }
+  ];
 
   constructor() { }
 
   ngOnInit() {
+    // console.log(this.contacts);
   }
 
 }
--- src/app/member/member.module.ts
+++ src/app/member/member.module.ts
@@ -6,17 +6,18 @@ import {LoginComponent} from './login/login.component';
 import {SharedModule} from '../shared/shared.module';
 import { MemberListComponent } from './member-list/member-list.component';
 import { ContactComponent } from './contact/contact.component';
+import {NgPipesModule} from 'ngx-pipes';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     SharedModule,
-    MemberRoutingModule
+    MemberRoutingModule,
+    NgPipesModule
   ],
   exports: [],
-  declarations: [LoginComponent, MemberListComponent, ContactComponent],
-  providers: []
+  declarations: [LoginComponent, MemberListComponent, ContactComponent]
 })
 export class MemberModule {
 }
